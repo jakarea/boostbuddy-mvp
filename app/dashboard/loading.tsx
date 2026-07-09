724ea0c1ac21051e7ae5807b39c161e534718496
Jakarea <jakareaparvez@gmail.com>
2026-07-09 23:48:58 +0600

    i18n: translate loading screen message to English and Italian

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import React from "react";
 import { LoadingScreen } from "@/components/LoadingScreen";
+import { useTranslation } from "react-i18next";
 
 export default function DashboardLoading() {
-  return <LoadingScreen message="Loading page..." />;
+  const { t } = useTranslation("common");
+  return <LoadingScreen message={t("is_loading", { defaultValue: "is loading..." })} />;
 }
